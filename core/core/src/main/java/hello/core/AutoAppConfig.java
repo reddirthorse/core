a747fc4f7c730e5096ce2a230b2211b5cd9d8cb4
reddirthorse <operstu1@gmail.com>
2021-11-02 20:16:15 +0900

    Initial commit

--- core/core/src/main/java/hello/core/AutoAppConfig.java
+++ core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -2,8 +2,12 @@ package hello.core;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan
+@ComponentScan(
+        //basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)//예제 코드를 살리기 위해서 사용, Appconfig를 삭제하면 없어도 됨
 public class AutoAppConfig {
 }
